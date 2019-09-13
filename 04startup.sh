8ced225bd7200e0e8b8fd240b5ad821910c03994
tanyuanji <tanyuanji@yihexinda.com>
2019-09-13 19:04:04 +0800

    修改启动环境

--- 04startup.sh
+++ 04startup.sh
@@ -4,8 +4,8 @@
 #    $1:jar文件名（包含后缀名）
 #    注意：jar文件必须位于startup.sh目录的上一层目录。
 
-#启动参数
-JAVA_OPTS="-server -Xms200m -Xmx200m -Xmn50m -XX:MetaspaceSize=50m -XX:MaxMetaspaceSize=50m -Xverify:none -XX:+DisableExplicitGC -Dspring.profiles.active=dev -Dsun.misc.URLClassPath.disableJarChecking=true  -Djava.awt.headless=true"
+#启动参数 -Dspring.profiles.active=dev
+JAVA_OPTS="-server -Xms200m -Xmx200m -Xmn50m -XX:MetaspaceSize=50m -XX:MaxMetaspaceSize=50m -Xverify:none -XX:+DisableExplicitGC -Dsun.misc.URLClassPath.disableJarChecking=true  -Djava.awt.headless=true"
 
 arg_name=${1%*/}
 this_dir="$( cd "$( dirname "$0"  )" && pwd )"
@@ -17,13 +17,13 @@ jar_file="${this_dir}/${arg_name}/target/${arg_name}-1.0.0-SNAPSHOT.war"
 #父目录下jar文件存在
 if [ -f "${jar_file}" ]; then
     nohup java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &
-    echo "启动命令: java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &"
+    #echo "java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &"
 	  echo "启动 ${jar_file}，日志文件 tail -f ${arg_name}.out"
     exit 0
 else
     jar_file="${this_dir}/${arg_name}/target/${arg_name}-1.0.0-SNAPSHOT.jar"
 	  nohup java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &
-	  echo "nohup java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &"
+	  #echo "nohup java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &"
 	  echo "启动 ${jar_file}，日志文件 tail -f ${arg_name}.out"
     exit 0
 fi
