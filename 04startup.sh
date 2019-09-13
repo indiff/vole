f8dd3e2526c3b3f71f09fd90c4feda8d73eb694e
tanyuanji <tanyuanji@yihexinda.com>
2019-09-13 18:58:26 +0800

    modify the version

--- 04startup.sh
+++ 04startup.sh
@@ -12,7 +12,7 @@ this_dir="$( cd "$( dirname "$0"  )" && pwd )"
 parent_dir=`dirname "${this_dir}"`
 
 
-jar_file="${this_dir}/${arg_name}/target/${arg_name}-0.0.1-SNAPSHOT.war"
+jar_file="${this_dir}/${arg_name}/target/${arg_name}-1.0.0-SNAPSHOT.war"
 
 #父目录下jar文件存在
 if [ -f "${jar_file}" ]; then
@@ -21,14 +21,15 @@ if [ -f "${jar_file}" ]; then
 	  echo "启动 ${jar_file}，日志文件 tail -f ${arg_name}.out"
     exit 0
 else
-    jar_file="${this_dir}/${arg_name}/target/${arg_name}-0.0.1-SNAPSHOT.jar"
+    jar_file="${this_dir}/${arg_name}/target/${arg_name}-1.0.0-SNAPSHOT.jar"
 	  nohup java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &
 	  echo "nohup java $JAVA_OPTS -jar ${jar_file}  > ${arg_name}.out 2>&1 &"
 	  echo "启动 ${jar_file}，日志文件 tail -f ${arg_name}.out"
     exit 0
 fi
 
-
+#/home/vole/vole-config/target/vole-config-1.0.0-SNAPSHOT.jar
+#/home/vole/vole-config/target/vole-config-0.0.1-SNAPSHOT.jar
 
 
 #启动jar包；重定向标准错误输出到文件，丢掉标准输出
