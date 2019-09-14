27fa059398a5670e6cc1d717ff1adcf669d434db
tanyuanji <tanyuanji@yihexinda.com>
2019-09-14 17:52:57 +0800

    add some shell for the docker

--- 02package.sh
+++ 02package.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+# 打包項目
 JAVA_OPTS="-server -Xms1048m -Xmx1048m -XX:PermSize=556m -XX:MaxPermSize=556M"
 sudo mvn clean  package -U -B  -T 1C -DskipTests -Dmaven.compile.fork=true
